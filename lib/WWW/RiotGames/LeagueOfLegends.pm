6607e6f68437ad7231c1a8608358c03739aab0bc
Justin Hunter <justin.d.hunter@gmail.com>
2017-07-06 03:19:43 -0400

    overhaul for v3

--- lib/WWW/RiotGames/LeagueOfLegends.pm
+++ lib/WWW/RiotGames/LeagueOfLegends.pm
@@ -6,7 +6,8 @@ use LWP;
 use JSON;
 use URI;
 use Sub::Name;
-use Types::Standard qw(Str Int Enum InstanceOf);
+use Types::Standard qw(Str Int Enum InstanceOf Bool);
+use Function::Parameters;
 
 our $VERSION = 0.0001;
 $VERSION = eval $VERSION;
@@ -56,22 +57,6 @@ the same terms as the Perl 5 programming language system itself.
 
 =cut
 
-my %api_methods = (
-  champion => 1.2,
-  #championmaster => undef,
-  current_game => 1.0,
-  #featured_games => 1.0,
-  game => 1.3,
-  league => 2.5,
-  match => 2.2,
-  matchlist => 2.2,
-  static_data => 1.2,
-  stats => 1.3,
-  #status => 1.3,
-  summoner => 1.4,
-  team => 2.4,
-);
-
 has api_key => (
   is => 'ro',
   isa => Str,
@@ -80,9 +65,9 @@ has api_key => (
 
 has region => (
   is => 'ro',
-  isa => Enum[qw( br eune euw jp kr lan las na oce ru tr )],
+  isa => Enum[qw( na1 euw1 eun1 jp1 kr oc1 br1 la1 la2 ru tr1 pbe1 )],
   required => 1,
-  default => sub { 'na' },
+  default => sub { 'na1' },
 );
 
 has ua => (
@@ -92,7 +77,8 @@ has ua => (
 
 has api_url => (
   is => 'lazy',
-  default => sub { 'https://' . $_[0]->region . '.api.pvp.net' },
+  isa => Str,
+  default => sub { 'https://' . $_[0]->region . '.api.riotgames.com' },
 );
 
 has timeout => (
@@ -137,42 +123,148 @@ sub _build_ua {
 
 sub _build_json { JSON->new->utf8->allow_nonref }
 
-sub _request {
-  my $self = shift;
-  my $method = shift;
-  my %args = ref($_[0]) ? %{$_[0]} : @_;
+method champion_mastery( Int :$summoner_id, Int :$champion_id = 0, Str :$type = 'champion-masteries' ) {
+  # /lol/champion-mastery/v3/champion-masteries/by-summoner/{summonerId}
+  # /lol/champion-mastery/v3/champion-masteries/by-summoner/{summonerId}/by-champion/{championId}
+  # /lol/champion-mastery/v3/scores/by-summoner/{summonerId}
 
-  (my $api_method = $method) =~ s/_/-/g;
+  my $url = $champion_id ?
+    sprintf '%s/lol/champion-mastery/v%d/champion-masteries/by-summoner/%d/by-champion/%d', $self->api_url, 3, $summoner_id, $champion_id :
+    sprintf '%s/lol/champion-mastery/v%d/%s/by-summoner/%d', $self->api_url, 3, $type, $summoner_id;
+  $self->_request( $url );
+}
 
-  my $api_url = $self->api_url;
-  if ($method eq 'current_game') {
-    $api_url .= '/observer-mode/rest/consumer/getSpectatorGameInfo/' . $region2platform{$self->region}{id};
-  } else {
-    $api_url .= '/api/lol/';
+method champions( Int :$champion_id = 0 ) {
+  # /lol/platform/v3/champions
+  # /lol/platform/v3/champions/{id}
+
+  my $url = $champion_id ?
+    sprintf '%s/lol/platform/v%d/champions/%d', $self->api_url, 3, $champion_id :
+    sprintf '%s/lol/platform/v%d/champions', $self->api_url, 3;
+  $self->_request( $url );
+}
+
+method league( Str :$type = 'challengerleagues', Str :$queue = '', Int :$summoner_id = 0 ) {
+  # /lol/league/v3/challengerleagues/by-queue/{queue}
+  # /lol/league/v3/leagues/by-summoner/{summonerId}
+  # /lol/league/v3/masterleagues/by-queue/{queue}
+  # /lol/league/v3/positions/by-summoner/{summonerId}
+
+  my $url;
+  if ( $type eq 'challengerleagues' || $type eq 'masterleagues' ) {
+    $url = sprintf '%s/lol/league/v%d/%s/by-queue/%s', $self->api_url, 3, $type, $queue;
+  } elsif ( $type eq 'leagues' || $type eq 'positions' ) {
+    $url = sprintf '%s/lol/league/v%d/%s/by-summoner/%s', $self->api_url, 3, $type, $summoner_id;
   }
-  if ($method eq 'static_data') {
-    $api_method = delete $args{type};
-    $api_url .= 'static-data/';
+  $self->_request( $url );
+}
+
+method static_data( Str :$type, Int :$id = 0 ) {
+  # /lol/static-data/v3/champions
+  # /lol/static-data/v3/champions/{id}
+  # /lol/static-data/v3/items
+  # /lol/static-data/v3/items/{id}
+  # /lol/static-data/v3/language-strings
+  # /lol/static-data/v3/languages
+  # /lol/static-data/v3/maps
+  # /lol/static-data/v3/masteries
+  # /lol/static-data/v3/masteries/{id}
+  # /lol/static-data/v3/profile-icons
+  # /lol/static-data/v3/realms
+  # /lol/static-data/v3/runes
+  # /lol/static-data/v3/runes/{id}
+  # /lol/static-data/v3/summoner-spells
+  # /lol/static-data/v3/summoner-spells/{id}
+  # /lol/static-data/v3/versions
+
+  my $url = $id ?
+    sprintf '%s/lol/static-data/v%d/%s/%d', $self->api_url, 3, $type, $id :
+    sprintf '%s/lol/static-data/v%d/%s', $self->api_url, 3, $type;
+  $self->_request( $url );
+}
+
+method status() {
+  # /lol/status/v3/shard-data
+
+  my $url = sprintf '%s/lol/status/v%d/shard-data', $self->api_url, 3;
+  $self->_request( $url );
+}
+
+method masteries( Int :$summoner_id ) {
+  # /lol/platform/v3/masteries/by-summoner/{summonerId}
+
+  my $url = sprintf '%s/lol/platform/v%d/masteries/by-summoner/%d', $self->api_url, 3, $summoner_id;
+  $self->_request( $url );
+}
+
+method match( Int :$match_id = 0, Str :$type = 'matches', Int :$account_id = 0, Str :$tournament_code = '', Bool :$recent = 0 ) {
+  # /lol/match/v3/matches/{matchId}
+  # /lol/match/v3/matchlists/by-account/{accountId}
+  # /lol/match/v3/matchlists/by-account/{accountId}/recent
+  # /lol/match/v3/timelines/by-match/{matchId}
+  # /lol/match/v3/matches/by-tournament-code/{tournamentCode}/ids
+  # /lol/match/v3/matches/{matchId}/by-tournament-code/{tournamentCode}
+
+  my $url;
+  if ( $type eq 'matches' ) {
+    if ( $match_id ) {
+      $url = $tournament_code ?
+        sprintf '%s/lol/match/v%d/%s/%d/by-tournament-code/%s', $self->api_url, 3, $type, $match_id, $tournament_code :
+        sprintf '%s/lol/match/v%d/%s/%d', $self->api_url, 3, $type, $match_id;
+    } elsif ( $tournament_code ) {
+      $url = sprintf '%s/lol/match/v%d/%s/by-tournament-code/%s/ids', $self->api_url, 3, $type, $tournament_code;
+    }
+  } elsif ( $type eq 'matchlists' ) {
+    $url = $recent ? 
+      sprintf '%s/lol/match/v%d/%s/by-account/%d/recent', $self->api_url, 3, $type, $account_id :
+      sprintf '%s/lol/match/v%d/%s/by-account/%d', $self->api_url, 3, $type;
+  } elsif ( $type eq 'timelines' ) {
+    $url = sprintf '%s/lol/match/v%d/%s/by-match/%d', $self->api_url, 3, $type, $match_id;
   }
+  $self->_request( $url );
+}
 
-  $api_url .= $self->region . '/v' . $api_methods{$method} .'/' . $api_method unless $method eq 'current_game';
-  $api_url .= '/by-' . delete $args{by} if exists $args{by};
-  $api_url .= '/' . delete $args{id} if exists $args{id};
-  $api_url .= '/' . delete $args{type} if exists $args{type};
+method runes( Int :$summoner_id ) {
+  # /lol/platform/v3/runes/by-summoner/{summonerId}
 
-  my $uri = URI->new($api_url);
-  $uri->query_form(api_key => $self->api_key, %args);
+  my $url = sprintf '%s/lol/platform/v%d/runes/by-summoner/%d', $self->api_url, 3, $summoner_id;
+  $self->_request( $url );
+}
 
+method spectator( Int :$summoner_id = 0 ) {
+  # /lol/spectator/v3/active-games/by-summoner/{summonerId}
+  # /lol/spectator/v3/featured-games
+
+  my $url = $summoner_id ?
+    sprintf '%s/lol/spectator/v%d/active-games/by-summoner/%d', $self->api_url, 3, $summoner_id :
+    sprintf '%s/lol/spectator/v%d/featured-games';
+  $self->_request( $url );
+}
+
+method summoner( Int :$account_id = 0, Int :$summoner_id = 0, Str :$summoner_name ) {
+  # /lol/summoner/v3/summoners/by-account/{accountId}
+  # /lol/summoner/v3/summoners/by-name/{summonerName}
+  # /lol/summoner/v3/summoners/{summonerId}
+
+  my $url;
+  if ( $account_id ) {
+    $url = sprintf '%s/lol/summoner/v%d/summoners/by-account/%d', $self->api_url, 3, $account_id;
+  } elsif ( $summoner_id ) {
+    $url = sprintf '%s/lol/summoner/v%d/summoners/%d', $self->api_url, 3, $summoner_id;
+  } elsif ( $summoner_name ) {
+    $url = sprintf '%s/lol/summoner/v%d/summoners/by-name/%s', $self->api_url, 3, $summoner_name;
+  }
+  $self->_request( $url );
+}
+
+method _request( Str $url ) {
+  my $uri = URI->new( $url );
+  $uri->query_form( api_key => $self->api_key );
   warn $uri->as_string if $self->debug;
 
   my $req = HTTP::Request->new('GET', $uri->as_string);
   my $response = $self->request( $req );
   return $response->is_success ? $self->decode($response->content) : $response->status_line;
-}
-
-no strict 'refs';
-for my $method (keys %api_methods) {
-  *{$method} = subname $method => sub { shift->_request($method, @_) };
 }
 
 1;
